6b70a4844b4ed0da98c37b2f98790c47641f6ea9
thanishkabaikani <thanishkabaikani@gmail.comgit config --global user.email thanishkabaikani@gmail.com>
2025-07-03 16:42:01 +0530

    created steup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="Thanishka",
+    author_email="thanishkabaikani@gmail.com",
+    install_requires=[
+        "openai",
+        "langchain",
+        "streamlit",
+        "python-dotnet",
+        "PyPDF2"
+    ],
+    packages=find_packages()
+)
+
